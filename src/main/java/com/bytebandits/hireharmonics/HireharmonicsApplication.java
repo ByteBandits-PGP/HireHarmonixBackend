1cbaca47e690a9c3a5c5ca86e100f1e22fef4ac9
Vijani <vijani@ravindu-Inspiron-3542.home>
2023-09-05 09:37:19 +0200

    Setup project

---
+++ src/main/java/com/bytebandits/hireharmonics/HireharmonicsApplication.java
@@ -0,0 +1,13 @@
+package com.bytebandits.hireharmonics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireharmonicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireharmonicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytebandits/hireharmonics/HireharmonicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytebandits.hireharmonics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireharmonicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
