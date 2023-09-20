eccc297bff6d7c0c63ca77f46dc88973c7a5c3af
Ayşe Aker Tütüncü <akerayse@gmail.com>
2023-09-20 16:52:23 +0200

    security part 3

--- src/main/java/com/bytebandits/hireharmonics/controller/UserController.java
+++ src/main/java/com/bytebandits/hireharmonics/controller/UserController.java
@@ -3,12 +3,18 @@ package com.bytebandits.hireharmonics.controller;
 import com.bytebandits.hireharmonics.dto.UserDTO;
 import com.bytebandits.hireharmonics.model.User;
 import com.bytebandits.hireharmonics.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolderStrategy;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +28,12 @@ public class UserController {
     private final AuthenticationManager authenticationManager;
 
     private PasswordEncoder passwordEncoder;
+
+    private SecurityContextRepository securityContextRepository =
+            new HttpSessionSecurityContextRepository();
+
+    private final SecurityContextHolderStrategy securityContextHolderStrategy = SecurityContextHolder.getContextHolderStrategy();
+
 
     public UserController(UserService userService, AuthenticationManager authenticationManager) {
         this.userService = userService;
@@ -38,11 +50,15 @@ public class UserController {
         return ResponseEntity.ok("User registered successfully");
     }
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<String> login(@RequestBody UserDTO userDTO, HttpServletRequest request, HttpServletResponse response) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                 userDTO.email(), userDTO.password()));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
+        SecurityContext context =  securityContextHolderStrategy.createEmptyContext();
+        context.setAuthentication(authentication);
+        securityContextHolderStrategy.setContext(context);
+        securityContextRepository.saveContext(context, request, response);
         return ResponseEntity.ok("Login successful");
     }
     @PostMapping("/logout")
