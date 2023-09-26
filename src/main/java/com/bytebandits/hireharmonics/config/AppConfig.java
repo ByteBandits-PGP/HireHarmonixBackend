dbb060fa2615f570905376ef3746b67f4e65b78f
Saghi Paknemat <paknemat@gmail.com>
2023-09-26 10:47:59 +0200

    settingup bucket for uplaod CV

--- src/main/java/com/bytebandits/hireharmonics/config/AppConfig.java
+++ src/main/java/com/bytebandits/hireharmonics/config/AppConfig.java
@@ -3,6 +3,7 @@ package com.bytebandits.hireharmonics.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.http.apache.ApacheHttpClient;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
@@ -12,6 +13,7 @@ public class AppConfig {
     public S3Client s3Client() {
         return S3Client.builder()
                 .httpClientBuilder(ApacheHttpClient.builder())
+                .region(Region.EU_WEST_1)
                 .build();
     }
 }
--- src/main/java/com/bytebandits/hireharmonics/service/UploadCVService.java
+++ src/main/java/com/bytebandits/hireharmonics/service/UploadCVService.java
@@ -1,12 +1,21 @@
 package com.bytebandits.hireharmonics.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Service
 public class UploadCVService {
 
+    private final String bucketName;
+    private final S3Client s3Client;
+    public UploadCVService(S3Client s3Client, @Value("s3.bucket.name") String bucketName) {
+        this.s3Client= s3Client;
+        this.bucketName= bucketName;
+    }
 
     public void uploadCV() {
+
         System.out.println("Service uploadCV called");
     }
 }
